4d87fa710eb835dc749263795d97c470fe2250c9
Radics Ádám <radicsadam@Reaktor-2-es.local>
2024-02-19 17:18:06 +0100

    Mappa szerkezetben, mappák törölve, 'main.js' és 'App.vue' fájl szerkesztve.

--- ucp-frontend/src/main.js
+++ ucp-frontend/src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).use(router).mount('#app')
+const webPrefix = 'Los Santos RolePlay'
+const routes = []
+
+const Router = createRouter({ history: createWebHistory(), routes })
+const Application = createApp(App)
+
+Router
+.beforeEach(( toPage, fromPage, nextFunc ) => {
+  document.title = toPage['meta']['title']
+  nextFunc()
+})
+
+Application
+.use(Router)
+.mount('#app')
