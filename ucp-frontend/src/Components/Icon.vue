b258a7f0e04fb7f3e48178a9dd05e32633f32039
Radics Ádám <radicsadam@Reaktor-2-es.local>
2024-02-19 17:27:47 +0100

    A nem található oldal hozzáadva a nézetekhez.

---
+++ ucp-frontend/src/Components/Icon.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="icon-wrapper">
+    <div :style="`background-image: url(${require('@/Assets/' + iconPath + '.png')}`" />
+    <span v-if="iconText !== ''">{{ iconText }}</span>
+  </div>
+</template>
+  
+<script setup>
+  import { toRefs } from 'vue'
+
+  const Props = defineProps({
+    iconPath: String,
+    iconText: String
+  })
+
+  const {
+    iconPath,
+    iconText
+  } = toRefs(Props)
+</script>
+
+<style lang="scss" scoped>
+  .icon-wrapper {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+
+    div {
+      aspect-ratio: 1;
+
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: 100%;
+    }
+
+    span {
+      font-size: 18px;
+    }
+  }
+</style>
---
+++ ucp-frontend/src/Views/PageNotFound.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page_not_found-wrapper">
+    <IconComponent iconPath="Icons/maintenance_white" />
+
+    <span>Az oldal jelenleg karbantartás alatt van!</span>
+    <span>A fejlesztők dolgoznak a hibák, illetve a felmerülő problémák kijavításán.</span>
+  </div>
+</template>
+
+<script setup>
+  import IconComponent from '@/Components/Icon'
+</script>
+
+<style lang="scss" scoped>
+  .page_not_found-wrapper {
+    :nth-child(1) {
+      margin-bottom: 50px;
+    }
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    text-align: center;
+    font-size: 18px;
+    color: white;
+  }
+</style>
