4d299a1d1439d3f12250c8584c5fc0e92d6068ec
Radics Ádám <radicsadam@Reaktor-2-es.local>
2024-02-22 19:37:49 +0100

    Regisztrációnál a jelszó titkosítva az adatbázisba mentéskor.

--- ucp-backend/server.js
+++ ucp-backend/server.js
@@ -3,6 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 const dotEnv = require('dotenv').config()
+const argon2 = require('argon2')
 
 // Modules Declarations
 const expressApp = express()
@@ -33,7 +34,7 @@ expressApp.get('/API/news', (req, res) => {
 expressApp.post('/API/registerUser', (req, res) => {
   const requestData = req.body
 
-  mysqlPool.query(`SELECT username FROM accounts WHERE username = '${ requestData.username }'`, (err, results) => {
+  mysqlPool.query(`SELECT username FROM accounts WHERE username = '${ requestData.username }'`, async (err, results) => {
     if (err) {
       return res.send([500, err.message])
     }
@@ -42,7 +43,9 @@ expressApp.post('/API/registerUser', (req, res) => {
       return res.send([401, 'Ilyen felhasználó már van!'])
     }
 
-    mysqlPool.query(`INSERT INTO accounts (username, password, email) VALUES ('${ requestData.username }', '${ requestData.password }', '${ requestData.email }')`, (err, results) => {
+    const hashedPassword = await argon2.hash(requestData.password)
+
+    mysqlPool.query(`INSERT INTO accounts (username, password, email) VALUES ('${ requestData.username }', '${ hashedPassword }', '${ requestData.email }')`, (err) => {
       if (err) {
         return res.send([500, err.message])
       }
