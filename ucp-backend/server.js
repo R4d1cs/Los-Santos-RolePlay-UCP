9541573f995545001fc107773277598eb3906729
Radics Ádám <radicsadam@Reaktor-2-es.local>
2024-03-19 16:43:32 +0100

    Backend kommentezve, 'helper' funkciók megírva.

--- ucp-backend/server.js
+++ ucp-backend/server.js
@@ -1,180 +1,162 @@
 // Modules Imports
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql')
-const dotEnv = require('dotenv').config()
-const argon2 = require('argon2')
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql');
+const dotEnv = require('dotenv').config();
+const argon2 = require('argon2');
 
 // Modules Declarations
-const expressApp = express()
+const expressApp = express();
 
-expressApp.use(express.urlencoded({extended: true}))
-expressApp.use(cors())
-expressApp.use(express.json())
+// Middleware
+expressApp.use(express.urlencoded({ extended: true }));
+expressApp.use(cors());
+expressApp.use(express.json());
 
 // MYSQL Pool create with connection attributes
 const mysqlPool = mysql.createPool({
-  connectionLimit : 10,
-  host            : process.env.DBHOST,
-  user            : process.env.DBUSER,
-  password        : process.env.DBPASS,
-  database        : process.env.DBNAME,
-  timezone        : 'UTC'
-})
+  connectionLimit: 10,
+  host: process.env.DBHOST,
+  user: process.env.DBUSER,
+  password: process.env.DBPASS,
+  database: process.env.DBNAME,
+  timezone: 'UTC'
+});
 
+// Connection to MySQL
 mysqlPool.getConnection((err, conn) => {
   if (err) {
-    return console.error(err)
+    console.error(err);
+    return;
   }
-
-  console.log('MYSQL: Sikeres adatbázis csatlakozás!')
-})
+  console.log('MySQL connection successful!');
+});
 
 // GET Endpoints
 expressApp.get('/API/news', (req, res) => {
   mysqlPool.query('SELECT * FROM news', (err, results) => {
     if (err) {
-      return res.send([500, err.message])
+      console.error(err);
+      return res.status(500).json({ error: err.message });
     }
-
-    res.send([200, results])
-  })
-})
+    res.status(200).json(results);
+  });
+});
 
 // POST Endpoints
-expressApp.post('/API/loginUser', (req, res) => {
-  const requestData = req.body
+expressApp.post('/API/loginUser', async (req, res) => {
+  const requestData = req.body;
 
-  mysqlPool.query(`SELECT * FROM accounts WHERE BINARY username = '${ requestData.username }'`, async (err, accountResults) => {
-    if (err) {
-      return res.send([500, err.message])
-    }
+  try {
+    const accountResults = await getUserAccount(requestData.username);
+    if (accountResults.length === 0)
+      return res.status(401).json({ message: 'No such user exists in the system!' });
 
-    if (accountResults == 0) {
-      return res.send([401, 'Ilyen felhasználó nincsen a rendszerben!'])
-    }
+    const verifyHashedPassword = await argon2.verify(accountResults[0].password, requestData.password);
+    if (!verifyHashedPassword)
+      return res.status(401).json({ message: 'Incorrect password provided!' });
 
-    const verifyHashedPassword = await argon2.verify(accountResults[0].password, requestData.password)
+    const userObject = await getUserProfileData(accountResults[0].accID);
+    return res.status(200).json({ message: 'Login successful!', data: userObject });
+  } catch (error) {
+    console.error('Error logging in user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-    if (!verifyHashedPassword) {
-      return res.send([401, 'Helytelen jelszót adtál meg!'])
-    }
-    
-    mysqlPool.query(`SELECT * FROM characters WHERE accID = '${ accountResults[0].accID }'`, async (err, charResults) => {
-      if (charResults == 0) {
-        const userObject = {
-          accountData: {
-            accID: accountResults[0].accID,
-            username: accountResults[0].username,
-            email: accountResults[0].email,
-            role: accountResults[0].role,
-            updatedAt: accountResults[0].updatedAt,
-            createdAt: accountResults[0].createdAt
-          }
-        }
-  
-        return res.send([200, 'Sikeres bejelentkezés!', userObject])
-      }
+expressApp.post('/API/registerUser', async (req, res) => {
+  const requestData = req.body;
 
-      const userObject = {
-        accountData: {
-          accID: accountResults[0].accID,
-          username: accountResults[0].username,
-          email: accountResults[0].email,
-          role: accountResults[0].role,
-          updatedAt: accountResults[0].updatedAt,
-          createdAt: accountResults[0].createdAt
-        },
-        charData: {
-          charID: charResults[0].charID,
-          charName: charResults[0].charName,
-          gameTime: charResults[0].gameTime,
-          cashBalance: charResults[0].cashBalance,
-          bankBalance: charResults[0].bankBalance,
-          updatedAt: charResults[0].updatedAt
-        }
-      }
+  try {
+    const existingUsername = await checkExistingUser('username', requestData.username);
+    if (existingUsername)
+      return res.status(401).json({ message: 'User already exists!' });
 
-      res.send([200, 'Sikeres bejelentkezés!', userObject])
-    })
-  })
-})
+    const existingEmail = await checkExistingUser('email', requestData.email);
+    if (existingEmail)
+      return res.status(401).json({ message: 'User with this email already exists!' });
 
-expressApp.post('/API/registerUser', (req, res) => {
-  const requestData = req.body
+    const hashedPassword = await argon2.hash(requestData.password);
+    await registerNewUser(requestData.username, hashedPassword, requestData.email);
+    return res.status(200).json({ message: 'Registration successful!' });
+  } catch (error) {
+    console.error('Error registering user:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-  mysqlPool.query(`SELECT username FROM accounts WHERE username = '${ requestData.username }'`, async (err, results) => {
-    if (err) {
-      return res.send([500, err.message])
-    }
+expressApp.post('/API/getUserProfile', async (req, res) => {
+  const requestData = req.body;
 
-    if (results != 0) {
-      return res.send([401, 'Ilyen felhasználó már van!'])
-    }
+  try {
+    const accountResults = await getUserAccount(requestData.accID);
+    const charResults = await getUserCharacters(accountResults[0].accID);
 
-    mysqlPool.query(`SELECT email FROM accounts WHERE email = '${ requestData.email }'`, async (err, results) => {
-      if (results != 0) {
-        return res.send([401, 'Ilyen e-mail címmel rendelkező felhasználó már van!'])
-      }
+    const userObject = {
+      accountData: {
+        accID: accountResults[0].accID,
+        username: accountResults[0].username,
+        email: accountResults[0].email,
+        role: accountResults[0].role,
+        updatedAt: accountResults[0].updatedAt,
+        createdAt: accountResults[0].createdAt
+      },
+      charData: charResults.length > 0 ? {
+        charID: charResults[0].charID,
+        charName: charResults[0].charName,
+        gameTime: charResults[0].gameTime,
+        cashBalance: charResults[0].cashBalance,
+        bankBalance: charResults[0].bankBalance,
+        updatedAt: charResults[0].updatedAt
+      } : null
+    };
 
-      const hashedPassword = await argon2.hash(requestData.password)
+    return res.status(200).json({ message: 'Successful profile data retrieval!', data: userObject });
+  } catch (error) {
+    console.error('Error fetching user profile data:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
-      mysqlPool.query(`INSERT INTO accounts (username, password, email) VALUES ('${ requestData.username }', '${ hashedPassword }', '${ requestData.email }')`, (err) => {
-        if (err) {
-          return res.send([500, err.message])
-        }
+// Helper Functions
+async function getUserAccount(username) {
+  return new Promise((resolve, reject) => {
+    mysqlPool.query(`SELECT * FROM accounts WHERE BINARY username = '${username}'`, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+}
 
-        res.send([200, 'Sikeres regisztráció!'])
-      })
-    })
-  })
-})
+async function getUserCharacters(accID) {
+  return new Promise((resolve, reject) => {
+    mysqlPool.query(`SELECT * FROM characters WHERE accID = '${accID}'`, (err, results) => {
+      if (err) reject(err);
+      resolve(results);
+    });
+  });
+}
 
-expressApp.post('/API/getUserProfile', (req, res) => {
-  const requestData = req.body
+async function checkExistingUser(field, value) {
+  return new Promise((resolve, reject) => {
+    mysqlPool.query(`SELECT ${field} FROM accounts WHERE ${field} = '${value}'`, (err, results) => {
+      if (err) reject(err);
+      resolve(results.length > 0);
+    });
+  });
+}
 
-  mysqlPool.query(`SELECT * FROM accounts WHERE accID = '${ requestData.accID }'`, async (err, accountResults) => {
-    mysqlPool.query(`SELECT * FROM characters WHERE accID = '${ accountResults[0].accID }'`, async (err, charResults) => {
-      if (charResults == 0) {
-        const userObject = {
-          accountData: {
-            accID: accountResults[0].accID,
-            username: accountResults[0].username,
-            email: accountResults[0].email,
-            role: accountResults[0].role,
-            updatedAt: accountResults[0].updatedAt,
-            createdAt: accountResults[0].createdAt
-          }
-        }
+async function registerNewUser(username, hashedPassword, email) {
+  return new Promise((resolve, reject) => {
+    mysqlPool.query(`INSERT INTO accounts (username, password, email) VALUES ('${username}', '${hashedPassword}', '${email}')`, (err) => {
+      if (err) reject(err);
+      resolve();
+    });
+  });
+}
 
-        return res.send([200, 'Sikeres profil adat lekérés!', userObject])
-      }
-
-      const userObject = {
-        accountData: {
-          accID: accountResults[0].accID,
-          username: accountResults[0].username,
-          email: accountResults[0].email,
-          role: accountResults[0].role,
-          updatedAt: accountResults[0].updatedAt,
-          createdAt: accountResults[0].createdAt
-        },
-        charData: {
-          charID: charResults[0].charID,
-          charName: charResults[0].charName,
-          gameTime: charResults[0].gameTime,
-          cashBalance: charResults[0].cashBalance,
-          bankBalance: charResults[0].bankBalance,
-          updatedAt: charResults[0].updatedAt
-        }
-      }
-
-      res.send([200, 'Sikeres profil adat lekérés!', userObject])
-    })
-  })
-})
-
-// Backend listening on a specify port
-expressApp.listen(process.env.SERVER_PORT, () => {
-  console.log(`API: A backend szerver sikeresen elindult! http://localhost:${process.env.SERVER_PORT}`)
-})
+// Backend listening on a specific port
+const PORT = process.env.SERVER_PORT || 3000;
+expressApp.listen(PORT, () => {
+  console.log(`API: Backend server successfully started at http://localhost:${PORT}`);
+});
