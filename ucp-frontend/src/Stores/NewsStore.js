1075b24471939e4ab9b66a2a07a0d14d841a5cb7
Radics Ádám <radicsadam@Reaktor-2-es.local>
2024-03-21 18:10:48 +0100

    'NewsStore.js' fájlban felesleges funkció kivéve.

--- ucp-frontend/src/Stores/NewsStore.js
+++ ucp-frontend/src/Stores/NewsStore.js
@@ -7,24 +7,6 @@ export const useNewsStore = defineStore('NewsStore', () => {
   // DATA/STATE
   const allNews = ref(null)
 
-  // METHODS/SETTERS
-  const updateNews = async (newsTable) => {
-    try {
-      const responseData = await fetch(`${serverURL}/news`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newsTable)
-      })
-      const responseJson = await responseData.json()
-      return responseJson
-    } catch (err) {
-      console.error(`Error fetching "news" table! (Err: ${err})`)
-      throw new Error('Internal server error')
-    }
-  }
-
   // COMPUTED/GETTERS
   const getNews = computed(async () => {
     try {
@@ -47,7 +29,6 @@ export const useNewsStore = defineStore('NewsStore', () => {
   // Exports what we want to access in the future
   return {
     allNews,
-    updateNews,
     getNews
   }
 }, { persist: true })
